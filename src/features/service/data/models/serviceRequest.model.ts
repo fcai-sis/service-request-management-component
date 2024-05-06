8d50a934a257f8442c5b1484a1220314fb7f96a6
Amr Khaled <lewolfje@gmail.com>
2024-05-06 23:08:58 +0300

    create service request model

---
+++ src/features/service/data/models/serviceRequest.model.ts
@@ -0,0 +1,46 @@
+import { studentModelName } from "@fcai-sis/shared-models";
+import mongoose, { InferSchemaType, Schema } from "mongoose";
+
+const ServiceStatus = ["pending", "completed", "cancelled"] as const;
+export type ServiceStatusType = (typeof ServiceStatus)[number];
+
+const ServiceRequestSchema = new mongoose.Schema({
+  serviceName: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ServiceStatus,
+    required: true,
+    default: "pending",
+  },
+  studentId: {
+    type: Schema.Types.ObjectId,
+    ref: studentModelName,
+    required: true,
+  },
+  message: {
+    type: String,
+    default: null,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  claimAt: {
+    type: Date,
+    default: null,
+  },
+});
+
+type ServiceRequestType = InferSchemaType<typeof ServiceRequestSchema>;
+
+const serviceRequestModelName = "ServiceRequest";
+
+const ServiceRequestModel = mongoose.model<ServiceRequestType>(
+  serviceRequestModelName,
+  ServiceRequestSchema
+);
+
+export default ServiceRequestModel;
