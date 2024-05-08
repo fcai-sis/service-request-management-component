4c198708928ecf12e08365974dc3930418343182
Amr Khaled <lewolfje@gmail.com>
2024-05-08 13:44:04 +0300

    add accept, reject, update, delete routes

--- src/features/service/serviceRequest.routes.ts
+++ src/features/service/serviceRequest.routes.ts
@@ -8,6 +8,13 @@ import validateCreateServiceRequestMiddleware from "./logic/middlewares/validate
 import { paginationQueryParamsMiddleware } from "@fcai-sis/shared-middlewares";
 import ensureServiceRequestIdInParamsMiddleware from "./logic/middlewares/ensureRequestIdInParams.middleware";
 import { upload } from "../../app";
+import acceptServiceRequestHandler from "./logic/handlers/acceptServiceRequest.handler";
+import validateAcceptServiceRequestMiddleware from "./logic/middlewares/acceptServiceRequest.middleware";
+import validateRejectServiceRequestMiddleware from "./logic/middlewares/rejectServiceRequest.middleware";
+import rejectServiceRequestHandler from "./logic/handlers/rejectServiceRequest.handler";
+import validateUpdateServiceRequestMiddleware from "./logic/middlewares/validateUpdateServiceRequest.middleware";
+import updateServiceRequestHandler from "./logic/handlers/updateServiceRequest.handler";
+import deleteServiceRequestHandler from "./logic/handlers/deleteServiceRequest.handler";
 
 const serviceRequestRoutes = (router: Router) => {
   router.post(
@@ -30,6 +37,32 @@ const serviceRequestRoutes = (router: Router) => {
 
     ensureServiceRequestIdInParamsMiddleware,
     asyncHandler(readServiceByIdHandler)
+  );
+
+  router.patch(
+    "/accept/:serviceRequestId",
+    ensureServiceRequestIdInParamsMiddleware,
+    validateAcceptServiceRequestMiddleware,
+    asyncHandler(acceptServiceRequestHandler)
+  );
+
+  router.patch(
+    "/reject/:serviceRequestId",
+    ensureServiceRequestIdInParamsMiddleware,
+    validateRejectServiceRequestMiddleware,
+    asyncHandler(rejectServiceRequestHandler)
+  );
+
+  router.patch(
+    "/update/:serviceRequestId",
+    ensureServiceRequestIdInParamsMiddleware,
+    validateUpdateServiceRequestMiddleware,
+    asyncHandler(updateServiceRequestHandler)
+  );
+  router.delete(
+    "/delete/:serviceRequestId",
+    ensureServiceRequestIdInParamsMiddleware,
+    asyncHandler(deleteServiceRequestHandler)
   );
 };
 
