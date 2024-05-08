42913beab9f6562e9e90087f548f36dbfbe4d158
Amr Khaled <lewolfje@gmail.com>
2024-05-08 12:31:03 +0300

    add upload file

--- src/features/service/logic/handlers/createServiceRequest.handler.ts
+++ src/features/service/logic/handlers/createServiceRequest.handler.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import ServiceRequestModel, {
   ServiceStatusType,
 } from "../../data/models/serviceRequest.model";
+import { cloudinary } from "../../../../database";
 
 /**
  * Handler for creating a new service request
@@ -12,21 +13,34 @@ type HandlerRequest = Request<
   {},
   {
     serviceName: string;
-    status: ServiceStatusType;
     studentId: string;
-    imgAttachment: string;
     message: string;
   }
 >;
 
 const handler = async (req: HandlerRequest, res: Response) => {
-  const { serviceName, status, studentId, imgAttachment, message } = req.body;
+  const { serviceName, studentId, message } = req.body;
+  const imgAttachment = req.file;
+
+  // Ensure an image attachment was provided
+  if (!imgAttachment) {
+    res.status(400).json({
+      summary: "Bad Request",
+      details: "No image attachment was provided in the request body",
+    });
+    return;
+  }
+  // Upload the strategy to cloudinary
+  const uploadResult = await cloudinary.uploader.upload(imgAttachment.path, {
+    folder: "attachments",
+    resource_type: "raw",
+  });
 
   const service = new ServiceRequestModel({
     serviceName,
-    status,
+    status: "pending",
     studentId,
-    imgAttachment,
+    imgAttachment: uploadResult.secure_url,
     message,
     createdAt: new Date(),
   });
