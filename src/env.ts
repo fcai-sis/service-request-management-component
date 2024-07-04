8cd126accd378a34cfc9a00173c50d072e34ad34
Amr Khaled <lewolfje@gmail.com>
2024-07-04 12:33:56 +0300

    add mail api

--- src/env.ts
+++ src/env.ts
@@ -7,6 +7,7 @@ export const env = {
   CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
   CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
   CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
+  MAIL_API_URL: process.env.MAIL_API_URL,
 };
 
 export const isDev = env.NODE_ENV === "development";
